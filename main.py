cae3130a8543df622b2edbcc8db601bf88ca16c8
Isaiah Collado <isaiah.collado@ust.edu.ph>
2026-09-04 02:03:03 +0800

    task 2 done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(" = " * 50) 
+print(" = " * 50)
 print(" Engineering Stress and Strain Calculator")
 print(" = " * 50)
 print(" PLease enter the requested values below.\n ")
@@ -25,7 +25,7 @@ print(" - " * 50)
 print(" Calculated Outputs: ")
 print(f" - Engineering Stress: {stress:,.2f}Pa")
 print(f" - Engineering Strain: {strain:.6f}")
-print( " = " * 50)       
+print( " = " * 50)
 
 #task 2
 def get_positive_float(prompt, allow_zero=False): #input validation
@@ -42,14 +42,14 @@ def get_positive_float(prompt, allow_zero=False): #input validation
     except ValueError:
       print("Error: Invalid input. Please enter a numerical value.")
 
-def select_material #material selection
+def select_material(): #material selection
   materials = {
-    "1": (Steel, 250.0, 200.0),
+    "1": ("Steel", 250.0, 200.0),
     "2": ("Aluminum", 95.0, 69.0),
-    "3": ("Titanium, 880.0, 114.0),
+    "3": ("Titanium", 880.0, 114.0),
   }
 
-  print("\n---Material Selection---)
+  print("\n---Material Selection---")
   print("1. Steel (Yield Strength: 250 MPa, Young's Modulus: 200 GPa)")
   print("2. Aluminum (Yield Strength: 95 MPa, Young's Modulus: 69 GPa)")
   print("3. Titanium (Yield Strength: 880 MPa, Young's Modulus: 114 GPa)")
@@ -73,63 +73,63 @@ def run_calculator():
     print("Stress, Stress, and Safety Analysis Calculator")
     print(" = " * 20)
 
-#material selection stuff
-mat_name, yield_strength_mpa, youngs_module_gpa = select_material()
+    #material selection stuff
+    mat_name, yield_strength_mpa, youngs_module_gpa = select_material()
 
-#input validation time
-print("\nPlease enter the requested values below.\n")
-applied_force = get_positive_float("Enter applied force (F) in Newton (N): ")
-cross_sectional_area = get_positive_float("Enter cross sectional area (A) in square meter [m^2]: ")
-original_length = get_positive_float("Enter original length in meters [m]: ")
-change_in_length = get_positive_float("Enter change in length in meters [m]: ", allow_zero=True)
+    #input validation time
+    print("\nPlease enter the requested values below.\n")
+    applied_force = get_positive_float("Enter applied force (F) in Newton (N): ")
+    cross_sectional_area = get_positive_float("Enter cross sectional area (A) in square meter [m^2]: ")
+    original_length = get_positive_float("Enter original length in meters [m]: ")
+    change_in_length = get_positive_float("Enter change in length in meters [m]: ", allow_zero=True)
 
-#calculations
-stress_pa = applied_force / cross_sectional_area
-stress_mpa = stress_pa / 1e6
-strain = change_in_length / original_length
+    #calculations
+    stress_pa = applied_force / cross_sectional_area
+    stress_mpa = stress_pa / 1e6
+    strain = change_in_length / original_length
 
-#analysis and factor of safety
-fos = yield_strength_mpa / stress_mpa
+    #analysis and factor of safety
+    fos = yield_strength_mpa / stress_mpa
 
-if fos >= 1.2:
-    status = "SAFE"
-elif 1.0 <= fos < 1.2:
-    status = "CAUTION - Loading near material yield point"
-else:
-    status = "UNSAFE - Material failure / yielding likely!"
+    if fos >= 1.2:
+        status = "SAFE"
+    elif 1.0 <= fos < 1.2:
+        status = "CAUTION - Loading near material yield point"
+    else:
+        status = "UNSAFE - Material failure / yielding likely!"
 
-#output display
-print("\n" + "=" * 50)
-print("Calculation results")
-print("=" * 50)
+    #output display
+    print("\n" + "=" * 50)
+    print("Calculation results")
+    print("=" * 50)
 
-print("Material Info:")
-print(f" - Material Selected: {mat_name}")
-print(f" - Yield Strength: {yield_strength_mpa:,.2f} MPa")
-print(f" - Young's Modulus: {youngs_modulus_gpa:,.2f} GPa")
+    print("Material Info:")
+    print(f" - Material Selected: {mat_name}")
+    print(f" - Yield Strength: {yield_strength_mpa:,.2f} MPa")
+    print(f" - Young's Modulus: {youngs_module_gpa:,.2f} GPa")
 
-print("Input Parameters:")
-print(f" - Applied Force: {applied_force:,.2f} N")
-print(f" - Cross Sectional Area: {cross_sectional_area:.6f} m^2")
-print(f" - Original Length: {original_length:.4f} m")
-print(f" - Change in Length: {change_in_length:.6f} m")
+    print("Input Parameters:")
+    print(f" - Applied Force: {applied_force:,.2f} N")
+    print(f" - Cross Sectional Area: {cross_sectional_area:.6f} m^2")
+    print(f" - Original Length: {original_length:.4f} m")
+    print(f" - Change in Length: {change_in_length:.6f} m")
 
-print("Calculated Outputs:")
-print(f" - Engineering Stress:    {stress_mpa:,.2f} MPa ({stress_pa:,.2f} Pa)")
-print(f" - Engineering Strain:    {strain:.6f:}")
-print(" - " * 25)
+    print("Calculated Outputs:")
+    print(f" - Engineering Stress:    {stress_mpa:,.2f} MPa ({stress_pa:,.2f} Pa)")
+    print(f" - Engineering Strain:    {strain:.6f}")
+    print(" - " * 25)
 
-print("Safety Analysis:")
-print(f" - Factor of Safety:      {fos:.2f}")
-print(f" - Safety Status:         {status}")
-print(" = " * 50)
+    print("Safety Analysis:")
+    print(f" - Factor of Safety:      {fos:.2f}")
+    print(f" - Safety Status:         {status}")
+    print(" = " * 50)
 
 def main():
     while True:
       try:
           run_calculator()
       except KeyboardInterrupt:
-          print("\n\nProgram force-closed by user. Existing grafully...")
+          print("\n\nProgram force-closed by user. Existing gracefully...")
           break
 
       repeat = input("\nWould you like to perform another calculation? (y/n): ").strip().lower()
@@ -137,5 +137,5 @@ def main():
           print("Thank you for using the calculator. Program terminated.")
           break
 
-if__name__ == "__main__":
+if __name__ == "__main__":
   main()
